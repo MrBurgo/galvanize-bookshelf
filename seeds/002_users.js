136e77aff75eb72345499613390e9fc2ff6f9901
Nathan Burgo <nthnbrg185@gmail.com>
2018-04-04 09:52:01 -0600

    User seed test passing

--- seeds/002_users.js
+++ seeds/002_users.js
@@ -1,13 +1,19 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex('users').del()
+    .then(() => {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('users').insert({
+          id: 1,
+          first_name: 'Joanne',
+          last_name: 'Rowling',
+          email: 'jkrowling@gmail.com',
+          hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',
+          created_at: new Date('2016-06-29 14:26:16 UTC'),
+          updated_at: new Date('2016-06-29 14:26:16 UTC')
+        }),
       ]);
     });
 };
