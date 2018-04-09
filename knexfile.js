07754ce54f65dd0230989248eafd87c62cc7ab55
Nathan Burgo <nthnbrg185@gmail.com>
2018-04-09 10:46:10 -0600

    Added heroku necessary files/scripts

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,8 @@ module.exports = {
     connection: 'postgres://localhost/bookshelf_dev'
   },
 
-  production: {}
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
